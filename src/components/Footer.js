ad27ec450ec8a4b43c4673012e4172f21b900506
joshburer <joshburer@gmail.com>
2020-12-21 21:17:07 -0500

    Fix scrollbar on page load

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { ReactComponent as Email} from "../static/icons/email.svg";
 
 export default function Footer() {
   return (
-    <footer id="footer" className="fade-in">
+    <footer id="footer" className="fade-in--no-move">
       <div className="social-links">
         <a className="link-button" href="https://github.com/joshburer">
           <Github /><span>Github</span>
