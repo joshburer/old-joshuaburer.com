a9ea38be41fe975ce8f53601042aa83a4695e763
joshburer <joshburer@gmail.com>
2020-12-26 16:28:17 -0500

    Footer modularity

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,32 @@
 import "./css/Footer.scss";
 // Icon imports
-import { ReactComponent as Github} from "../static/icons/github.svg";
-import { ReactComponent as Email} from "../static/icons/email.svg";
+import { ReactComponent as Github } from "../static/icons/github.svg";
+import { ReactComponent as Email } from "../static/icons/email.svg";
 
 export default function Footer() {
   return (
     <footer id="footer" className="fade-in--no-move">
       <div className="social-links">
-        <a className="link-button" href="https://github.com/joshburer">
-          <Github /><span>Github</span>
-        </a>
-        <a className="link-button" href="mailto:contact@joshuaburer.com">
-          <Email /><span>Email</span>
-        </a>
+        <SocialLink
+          text="Github"
+          link="https://github.com/joshburer"
+          icon={<Github />}
+        />
+        <SocialLink
+          text="Email"
+          link="mailto:contact@joshuaburer.com"
+          icon={<Email />}
+        />
       </div>
     </footer>
   );
 }
+
+var SocialLink = (props) => {
+  return (
+    <a className="link-button" href={props.link}>
+      {props.icon}
+      {props.text}
+    </a>
+  );
+};
