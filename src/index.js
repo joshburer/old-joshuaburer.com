4b80adfd7ca9000dc2218bc410a118098fb35618
joshburer <joshburer@gmail.com>
2021-07-31 16:48:15 -0400

    Codesplitting and cleanup

--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
 
 // Components
-import Navbar from "./components/Navbar.js";
-import Footer from "./components/Footer.js";
-
-// Pages
-import Home from "./pages/Home.js";
-import Info from "./pages/Info.js";
-import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // Page Routing
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+const Info = React.lazy(() => import("./pages/Info"));
+const Projects = React.lazy(() => import("./pages/Projects"));
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/info" exact component={Info} />
+        <React.Suspense fallback={null}>
+          <Route path="/" exact component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/info" exact component={Info} />
+        </React.Suspense>
       </Switch>
     </Router>
     <Footer />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
